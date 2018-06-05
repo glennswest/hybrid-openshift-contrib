35781d9b7d08e432f58dcda9ee399a76ba2ba41a
glennswest <glennswest@neuralcloudcomputing.com>
2018-06-05 09:23:13 -0500

    Remove version tags

--- reference-architecture/azure-ansible/3.9/bastion.sh
+++ reference-architecture/azure-ansible/3.9/bastion.sh
@@ -254,7 +254,7 @@ new_nodes
 new_masters
 
 [OSEv3:vars]
-openshift_vers=v3_9
+#openshift_vers=v3_9
 #osm_controller_args={'cloud-provider': ['azure'], 'cloud-config': ['/etc/azure/azure.conf']}
 #osm_api_server_args={'cloud-provider': ['azure'], 'cloud-config': ['/etc/azure/azure.conf']}
 #openshift_node_kubelet_args={'cloud-provider': ['azure'], 'cloud-config': ['/etc/azure/azure.conf'], 'enable-controller-attach-detach': ['true']}
@@ -273,7 +273,7 @@ openshift_master_api_port="{{ console_port }}"
 openshift_master_console_port="{{ console_port }}"
 openshift_override_hostname_check=true
 osm_use_cockpit=false
-openshift_release=v3.9
+#openshift_release=v3.9
 #openshift_cloudprovider_kind=azure
 openshift_node_local_quota_per_fsgroup=512Mi
 azure_resource_group=${RESOURCEGROUP}
